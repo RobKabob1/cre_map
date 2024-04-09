b42e11970d4343c256dfe55a897676648e79fe1d
rwp005 <rwp005@gmail.com>
2024-04-09 12:02:22 -0500

    created settings screen

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cre_map/utils/settings_options.dart';
 import 'package:flutter/material.dart';
 
 class SettingsScreen extends StatelessWidget {
@@ -5,6 +6,112 @@ class SettingsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    var keys = settingsOptions.keys.toList();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Profile & Settings'),
+        centerTitle: true,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const Padding(
+            padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
+            child: Row(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: CircleAvatar(
+                    backgroundColor: Colors.grey,
+                    backgroundImage: AssetImage(
+                      'assets/images/placeholder_profile.png',
+                    ),
+                    radius: 40,
+                  ),
+                ),
+                Padding(padding: EdgeInsets.all(10)),
+                Expanded(
+                  flex: 4,
+                  child: Center(
+                    child: Text(
+                      "placeholder.email@gmail.com",
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          InkWell(
+            onTap: () {},
+            child: Container(
+              decoration: const BoxDecoration(color: Colors.redAccent),
+              height: 50,
+              child: const Center(
+                child: Text("Subscribed"),
+              ),
+            ),
+          ),
+          //const Divider(),
+          Expanded(
+            child: ListView.builder(
+              // shrinkWrap: true,
+              itemCount: settingsOptions.length,
+              itemBuilder: (context, index) {
+                return SizedBox(
+                  height: 40,
+                  child: InkWell(
+                    onTap: () {},
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                      child: Expanded(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Icon(settingsOptions[keys[index]]),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            Text(keys[index]),
+                            Expanded(
+                              child: Container(),
+                            ),
+                            const Icon(Icons.navigate_next),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Privacy Policy",
+                  style: TextStyle(color: Colors.grey),
+                ),
+                Text(
+                  " - ",
+                  style: TextStyle(color: Colors.grey),
+                ),
+                Text(
+                  "Terms of Service",
+                  style: TextStyle(color: Colors.grey),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
---
+++ lib/utils/settings_options.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+Map<String, IconData> settingsOptions = {
+  'Account Information': Icons.account_circle_outlined,
+  'Sharing Message': Icons.message_outlined,
+  'White Label': Icons.label_important_outline,
+  'Property Templates': Icons.warehouse_outlined,
+  'Billing': Icons.attach_money_outlined,
+  'Send Feedback': Icons.mail_outline_outlined,
+  'Help Center': Icons.help_outline_outlined,
+  'Youtube': Icons.ondemand_video_outlined,
+};
