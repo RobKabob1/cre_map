cc736f81593ee8fa90a8f7affa035be068992fff
rwp005 <rwp005@gmail.com>
2024-06-07 14:38:40 -0500

    Completed Property Info 1.0 & Updated Pictures

---
+++ lib/utils/image_list.dart
@@ -0,0 +1,8 @@
+final List<String> imgList = [
+  'https://images1.loopnet.com/i2/hmrJzZQU-NGPH4J6AF-N8mUb7I0EyKwq3AoH-62SenQ/112/image.jpg',
+  'https://images1.loopnet.com/i2/7oadFsglcxBpIcT61pxpJxcSIq2J5YyoNtCWDZocqMw/112/image.jpg',
+  'https://images1.loopnet.com/m2/2aen9dWpSxZoU6cjMN2hn7DY-HXC1Lj4_D3WDOIAalhC7of4gPq5WiZSOdVFJ4n4I3zn7awlF28yeKivl6N1qlt2FEGtCuLTyXPniuHG4VmpDEtpJaB2GYeDoXVxITQIC-Z2kQjE-0d2s0PKfW82_A/H450W610/map.jpg',
+  'https://cdn.commercialcafe.com/images/99EF7A51-64F5-4DEB-A1D1-E3863F6A1626/10798.jpg',
+  'https://cdn.commercialcafe.com/images/C506267B-B60B-46AE-92EA-ADE24871197F/10799.jpg',
+  'https://cdn.commercialcafe.com/images/053CFAA1-3F65-43F3-B474-1F5AD766E061/10800.jpg',
+];
---
+++ lib/utils/property_info_list.dart
@@ -0,0 +1,8 @@
+final Map<String, String> propertyMap = {
+  "Property Address": "1600 Smith St Houston TX 77002",
+  "Property Name": "Continental Building",
+  "Floors": "19",
+  "Class": "A",
+  "Avg. Unit Size": "8,991 SF",
+  "Land Size": "20,000 SF",
+};
